a7007580b61a3f1f6b32faddd53f399871304445
GiorgiLomaia24 <admin@admins-MacBook-Pro.local>
2024-03-29 20:06:12 +0400

    End of Section 6

---
+++ src/AuctionService/Consumers/AuctionFinishedConsumer.cs
@@ -0,0 +1,30 @@
+﻿using AuctionService.Data;
+using AuctionService.Entities;
+using Contracts;
+using MassTransit;
+
+namespace AuctionService;
+
+public class AuctionFinishedConsumer : IConsumer<AuctionFinished>
+{
+    private readonly AuctionDbContext _DbContext;
+    public AuctionFinishedConsumer(AuctionDbContext DbContext)
+    {
+        _DbContext = DbContext;
+        
+    }
+    public async Task Consume(ConsumeContext<AuctionFinished> context)
+    {
+         Console.WriteLine("---------->Auction Finished Consumer");
+        var auction = await _DbContext.Auctions.FindAsync(context.Message.AuctionId);
+
+        if(context.Message.ItemSold){
+            auction.Winner = context.Message.Winner;
+            auction.SoldAmount = context.Message.Amount;
+        }
+
+        auction.Status = auction.SoldAmount > auction.ReservePrice ? Status.Finished : Status.ReserveNotMet;
+
+        await _DbContext.SaveChangesAsync();
+    }
+}
---
+++ src/AuctionService/Consumers/BidPlacedConsumer.cs
@@ -0,0 +1,32 @@
+﻿using AuctionService.Data;
+using Contracts;
+using MassTransit;
+
+namespace AuctionService;
+
+public class BidPlacedConsumer : IConsumer<BidPlaced>
+{
+    private readonly AuctionDbContext _DbContext;
+    public BidPlacedConsumer(AuctionDbContext DbContext)
+    {
+
+        _DbContext = DbContext;
+        
+    }
+    public async Task Consume(ConsumeContext<BidPlaced> context)
+    {
+        Console.WriteLine("---------->Bid PLaced Consumer");
+        var auction = await _DbContext.Auctions.FindAsync(context.Message.AuctionId);
+
+        if(auction.CurrentHighBid == null || context.Message.BidStatus.Contains("Accepted") && context.Message.Amount > auction.CurrentHighBid)
+        {
+
+            auction.CurrentHighBid = context.Message.Amount;
+
+            await _DbContext.SaveChangesAsync();
+
+        }
+
+
+    }
+}
